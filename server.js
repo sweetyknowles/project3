cf0f9c1b5e4316409ea3a63cf9004c8aedb7757f
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-03-11 21:26:48 -0400

    fist commit to test push to git

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const app = express();
+
+// connect to mongoose
+mongoose.connect(process.env.MONGODB_URI);
+
+// Logging info about database
+const connection = mongoose.connection;
+connection.on("connected", () => {
+  console.log("Mongoose connected successfully");
+});
+connection.on("error", err => {
+  console.log("Mongoose Error: ", err);
+});
+
+// apply middleware
+
+app.use(logger("dev"));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello World")
+})
+
+//bringing in the react client side.
+// app.use(express.static(`${__dirname}/client/build`));
+
+// const creatureController = require("./controllers/creatureController");
+// app.use("/api/creatures", creatureController);
+
+// Exposes the Static Javascript HTML and CSS we need to run the app
+
+//below your api routes
+// app.get("/*", (req, res) => {
+//   res.sendFile(`${__dirname}/client/build/index.html`);
+// });
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Application is listening on Port" + PORT);
+});
